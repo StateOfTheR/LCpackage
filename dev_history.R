54b6ef16d271693a54159249d74d75b3f11cdfef
Antoine Bichat <a.bichat@yahoo.fr>
2020-04-17 09:42:40 +0200

    description

---
+++ dev_history.R
@@ -0,0 +1,19 @@
+library(devtools)
+
+#### Only one time ####
+
+# use_git()
+
+# use_r("calculations")
+# use_r("date")
+
+
+#### Used regularly ####
+
+load_all()
+
+document()
+
+use_tidy_description()
+attachment::att_to_description()
+
